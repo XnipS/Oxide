a622106c900f536bdb93530c7391b2c893c3644d
XnipS <christoclan12@gmail.com>
2022-06-30 13:24:26 +1000

    Swapped masks to sliders

--- Assets/Scripts/ui_hud.cs
+++ Assets/Scripts/ui_hud.cs
@@ -7,26 +7,25 @@ public class ui_hud : MonoBehaviour
     {
         health = 0, water = 1, hunger = 2
     }
-    public RectMask2D healthMask;
-    public RectMask2D hungerMask;
-    public RectMask2D waterMask;
+    public Slider healthMask;
+    public Slider hungerMask;
+    public Slider waterMask;
 
     public void UpdateStatusHud(statusType type, float amount)
     {
         ui_manager manager = FindObjectOfType<ui_manager>();
         int max = manager.maxStats[type];
-        float val = (215 - ((max / 215) * amount));
-        Debug.Log(val);
+        Debug.Log(amount);
         switch (type)
         {
             case (statusType.health):
-                healthMask.padding = new Vector4(0, 0, val, 0);
+                healthMask.value = amount;
                 break;
             case (statusType.water):
-                waterMask.padding = new Vector4(0, 0, val, 0);
+                waterMask.value = amount;
                 break;
             case (statusType.hunger):
-                hungerMask.padding = new Vector4(0, 0, val, 0);
+                hungerMask.value = amount;
                 break;
         }
     }
